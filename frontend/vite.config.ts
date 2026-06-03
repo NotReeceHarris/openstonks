27b2da59fb4e248ec6e17e45662efcf9ebe9d3a7
Reece Harris <reece@codegalaxy.co.uk>
2026-06-03 11:41:30 +0100

    Initialize Wails Svelte-TS template

---
+++ frontend/vite.config.ts
@@ -0,0 +1,11 @@
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        tailwindcss(),
+        svelte()
+    ]
+});
